1b8f45cc376ce3f8c92744091e991533607a3e86
samruddhi-19 <samruddhibarahate@gmail.com>
2026-05-14 13:52:06 +0530

    add icon to button

--- public/client.js
+++ public/client.js
@@ -1,7 +1,8 @@
 window.TrelloPowerUp.initialize({
   'board-buttons': function(t) {
     return [{
-      text: 'Download Attachments',
+      icon: 'https://cdn-icons-png.flaticon.com/512/724/724933.png',
+      text: 'Download',
       callback: function(t) {
         return t.popup({
           title: 'Downloader',
